94991639415c20b0c7bff6e35bbd274fdf0ed449
ikuo saito <ikuosaito1989@gmail.com>
2021-09-26 21:44:09 +0900

    Fix dotnet version

--- Startup.cs
+++ Startup.cs
@@ -10,7 +10,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 
 namespace dotnet_core_docker
 {
@@ -26,12 +25,7 @@ namespace dotnet_core_docker
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "dotnet_core_docker", Version = "v1" });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +34,6 @@ namespace dotnet_core_docker
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "dotnet_core_docker v1"));
             }
 
             app.UseHttpsRedirection();
